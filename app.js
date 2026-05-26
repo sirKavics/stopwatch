461b5c1b1ad27bda97571e30e9f902aef1e2e4c9
sirkavics <falf555@gmail.com>
2026-05-26 17:54:01 -0400

    minutes stopwatch functionality

--- app.js
+++ app.js
@@ -20,10 +20,28 @@ function resetTimer() {
 }
 
 function updateTimer() {
-    let millisElapsed = (Date.now() - startTime)
-    let secondsElapsed = Math.floor(millisElapsed / 1000);
+    let millisElapsed = (Date.now() - startTime);
+    let secondsElapsed = millisElapsed / 1000;
+    let minutesElapsed = secondsElapsed / 60;
 
-    timerMilliseconds.innerHTML = millisElapsed % 1000
-    timerSeconds.innerHTML = secondsElapsed
+    let minutesFormat = Math.floor(minutesElapsed);
+    let secondsFormat = Math.floor(secondsElapsed % 60);
+    let millisFormat = millisElapsed % 1000
+
+    if (minutesFormat.toString().length === 1) {
+        minutesFormat = "0" + minutesFormat;
+    }
+    if (secondsFormat.toString().length === 1) {
+        secondsFormat = "0" + secondsFormat;
+    }
+    if (millisFormat.toString().length < 3) {
+        millisFormat = millisFormat.toString().padStart(3, "0");
+    }
+    
+
+    timerMilliseconds.innerHTML = millisFormat;
+    timerSeconds.innerHTML = secondsFormat;
+    timerMinutes.innerHTML = minutesFormat;
+
     cancelId = requestAnimationFrame(updateTimer)
 }
