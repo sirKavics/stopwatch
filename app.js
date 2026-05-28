d382749eada32cd6a882a954058ba03d90570a19
sirkavics <falf555@gmail.com>
2026-05-28 17:30:24 -0400

    practice run reset

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-const timerMilliseconds = document.querySelector('.timer__milliseconds');
-const timerSeconds = document.querySelector('.timer__seconds');
-const timerMinutes = document.querySelector('.timer__minutes');
+// const timerMilliseconds = document.querySelector('.timer__milliseconds');
+// const timerSeconds = document.querySelector('.timer__seconds');
+// const timerMinutes = document.querySelector('.timer__minutes');
 
-let cancelId;
-let startTime;
-let savedTime = 0;
+// let cancelId;
+// let startTime;
+// let savedTime = 0;
 
-function startTimer() {
-    startTime = Date.now();
-    cancelId = requestAnimationFrame(updateTimer)
-}
+// function startTimer() {
+//     startTime = Date.now();
+//     cancelId = requestAnimationFrame(updateTimer)
+// }
 
-function stopTimer() {
-    savedTime = savedTime + Date.now() - startTime;
-    console.log(savedTime)
-    cancelAnimationFrame(cancelId);
-}
+// function stopTimer() {
+//     savedTime = savedTime + Date.now() - startTime;
+//     console.log(savedTime)
+//     cancelAnimationFrame(cancelId);
+// }
 
-function resetTimer() {
-    startTime = Date.now();
-    savedTime = 0;
+// function resetTimer() {
+//     startTime = Date.now();
+//     savedTime = 0;
 
-    timerMilliseconds.innerHTML = "000";
-    timerSeconds.innerHTML = "00";
-    timerMinutes.innerHTML = "00";
-}
+//     timerMilliseconds.innerHTML = "000";
+//     timerSeconds.innerHTML = "00";
+//     timerMinutes.innerHTML = "00";
+// }
 
-function updateTimer() {
-    let millisElapsed = savedTime + Date.now() - startTime;
-    let secondsElapsed = millisElapsed / 1000;
-    let minutesElapsed = secondsElapsed / 60;
+// function updateTimer() {
+//     let millisElapsed = savedTime + Date.now() - startTime;
+//     let secondsElapsed = millisElapsed / 1000;
+//     let minutesElapsed = secondsElapsed / 60;
 
-    let millisFormat = millisElapsed % 1000
-    let secondsFormat = Math.floor(secondsElapsed % 60);
-    let minutesFormat = Math.floor(minutesElapsed);
+//     let millisFormat = millisElapsed % 1000
+//     let secondsFormat = Math.floor(secondsElapsed % 60);
+//     let minutesFormat = Math.floor(minutesElapsed);
 
-    if (minutesFormat.toString().length === 1) {
-        minutesFormat = "0" + minutesFormat;
-    }
-    if (secondsFormat.toString().length === 1) {
-        secondsFormat = "0" + secondsFormat;
-    }
-    if (millisFormat.toString().length < 3) {
-        millisFormat = millisFormat.toString().padStart(3, "0");
-    }
+//     if (minutesFormat.toString().length === 1) {
+//         minutesFormat = "0" + minutesFormat;
+//     }
+//     if (secondsFormat.toString().length === 1) {
+//         secondsFormat = "0" + secondsFormat;
+//     }
+//     if (millisFormat.toString().length < 3) {
+//         millisFormat = millisFormat.toString().padStart(3, "0");
+//     }
     
 
-    timerMilliseconds.innerHTML = millisFormat;
-    timerSeconds.innerHTML = secondsFormat;
-    timerMinutes.innerHTML = minutesFormat;
+//     timerMilliseconds.innerHTML = millisFormat;
+//     timerSeconds.innerHTML = secondsFormat;
+//     timerMinutes.innerHTML = minutesFormat;
 
-    cancelId = requestAnimationFrame(updateTimer)
-}
+//     cancelId = requestAnimationFrame(updateTimer)
+// }
+
+// PRACTICE RUN
+
